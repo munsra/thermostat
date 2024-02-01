83393c188d8ac340cb095a30a950a5b65079eb48
piero.silvestri <piero.silvestri@overit.it>
2024-02-01 17:52:03 +0100

    Added thermostat component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IgxRadialGaugeModule } from 'igniteui-angular-gauges';
+import { ThermostatComponent } from './thermostat/thermostat.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThermostatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IgxRadialGaugeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/thermostat/thermostat.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thermostat',
+  templateUrl: './thermostat.component.html',
+  styleUrl: './thermostat.component.css'
+})
+export class ThermostatComponent implements OnInit{
+  @Input() minValue = 0;
+  @Input() maxValue = 30;
+  @Input() targetValue = 150;
+
+  ngOnInit(): void {
+    if(this.targetValue > this.maxValue){
+      this.targetValue = this.maxValue;
+    }
+  }
+
+}
